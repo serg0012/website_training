06aa6e496954c93f480e2dfe4cc3f813a131930d
Serhii Sydorenko <seregawz@gmail.com>
2023-05-14 17:09:06 +0300

    prettier used

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,14 @@
-const btnForm = document.querySelector('.btn-booking');
-const overlay = document.querySelector('.overlay');
-const form = document.querySelector('.callback-form');
+const btnForm = document.querySelector(".btn-booking");
+const overlay = document.querySelector(".overlay");
+const form = document.querySelector(".callback-form");
 // const btnsMoreInfo = document.querySelectorAll('.more__info');
-const body = document.querySelector('body');
-const scrollToTopBtn = document.getElementById('scroll-to-top');
+const body = document.querySelector("body");
+const scrollToTopBtn = document.getElementById("scroll-to-top");
 
-btnForm.addEventListener('click', () => {
-    overlay.style.display = 'flex';
-    body.style.overflow = 'hidden';
-})
+btnForm.addEventListener("click", () => {
+  overlay.style.display = "flex";
+  body.style.overflow = "hidden";
+});
 
 // btnsMoreInfo.forEach((btn) => {
 //     btn.addEventListener(('click'), () => {
@@ -17,45 +17,44 @@ btnForm.addEventListener('click', () => {
 //     })
 // })
 
-form.addEventListener('click', (e) => {
-    e.stopPropagation();
-})
+form.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
 
-overlay.addEventListener('click', () => {
-    overlay.style.display = 'none';
-    body.style.overflow = 'auto';
-})
+overlay.addEventListener("click", () => {
+  overlay.style.display = "none";
+  body.style.overflow = "auto";
+});
 
 // Переміщення до елементу з id booking
 function scrollToBooking() {
-    const booking = document.getElementById('booking');
-    booking.scrollIntoView({ behavior: "smooth" });
+  const booking = document.getElementById("booking");
+  booking.scrollIntoView({ behavior: "smooth" });
 }
 
 // Переміщення до елементу з id header
 function scrollToHeader() {
-    const header = document.getElementById('header')
-    header.scrollIntoView({ behavior: "smooth" })
+  const header = document.getElementById("header");
+  header.scrollIntoView({ behavior: "smooth" });
 }
 
 scrollToTopBtn.addEventListener("click", scrollToTop);
 
 // Функція, яка піднімає сторінку вгору
 function scrollToTop() {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 }
-
 
 // показуємо кнопку, коли користувач прокрутив сторінку до певної висоти
 window.addEventListener("scroll", () => {
-    const currentScroll = window.pageYOffset;
+  const currentScroll = window.pageYOffset;
 
-    if (currentScroll > 100) {
-      scrollToTopBtn.style.display = "block";
-    } else {
-        scrollToTopBtn.style.display = "none";
-    }
+  if (currentScroll > 100) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
 });
